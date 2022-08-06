e8ad55152a7a46a51a7dd8672d846eec5578de68
shaikshan <shaikroshan1997@gmail.com>
2022-08-06 12:16:49 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from typing import List
+
+
+# Declaring variables for setup function
+PROJECT_NAME = "Housing-Prediction"
+VERSION = "0.0.1"
+AUTHOR = "Roshan Zameer"
+DESCRIPTION = "This is my first ML Project of House-Prediction"
+PACKAGES = ["housing"]
+REQUIREMENTS_FILE_NAME = "requirements.txt"
+
+
+def get_requirements_list()->List[str]:
+    """
+    Description: This function returns list of requirements
+    mentioned in requirements.txt file.
+
+    return : This function will return  a list of names which contain  
+     in requirements.txt
+    """
+    with open(REQUIREMENTS_FILE_NAME) as requirements_file: 
+       return requirements_file.readlines()
+
+
+
+
+setup(name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESCRIPTION,
+packages=PACKAGES,
+install_requires = get_requirements_list() )
